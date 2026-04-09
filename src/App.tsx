5f483b61810a040c57c4716ef921d48109b1077d
Yatagaru <nap1903@gmail.com>
2026-04-09 13:51:18 -0300

    Corrections

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { Routes, Route } from "react-router" // Used to make a page be inserted in the routing (SPA)
-import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router"
 import ProductPage from './pages/ProductPage';
 import './index.css'
 function App(){
 
     return (
 
-        // Need to wrap the elements in the Routes to work
+       
         <Routes>
-            {/* Route path (URL, in this case is empty so index, or "/") is basically the page, and element is the component we want to show in that page */}
             <Route index element= {<HomePage/>}/>
             <Route path='/product/:id' element={<ProductPage/>}/>
         </Routes>
